c3eb6b00f80435daa307b318ab2d1c1896cbc1b3
Brian Elinsky <be2112@gmail.com>
2020-11-09 08:14:29 -0600

    Refactored DFS Tests
    - Created a BeforeEach method
    - Parameterized the tests

--- src/test/java/com/brianelinsky/graph/DepthFirstSearchTests.java
+++ src/test/java/com/brianelinsky/graph/DepthFirstSearchTests.java
@@ -1,84 +1,56 @@
 package com.brianelinsky.graph;
 
-import com.google.common.graph.Graph;
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DepthFirstSearchTests {
+  MutableGraph<Integer> tinyGraph;
 
-  private Graph<Integer> createTinyGraph() {
-    MutableGraph<Integer> graph = GraphBuilder.undirected().build();
+  @BeforeEach
+  private void createTinyGraph() {
+    tinyGraph = GraphBuilder.undirected().build();
     for (int i = 0; i < 7; i++) {
-      graph.addNode(i);
+      tinyGraph.addNode(i);
     }
-    graph.putEdge(0, 1);
-    graph.putEdge(0, 2);
-    graph.putEdge(0, 5);
-    graph.putEdge(1, 2);
-    graph.putEdge(2, 3);
-    graph.putEdge(2, 4);
-    graph.putEdge(3, 4);
-    graph.putEdge(3, 5);
-
-    return graph;
+    tinyGraph.putEdge(0, 1);
+    tinyGraph.putEdge(0, 2);
+    tinyGraph.putEdge(0, 5);
+    tinyGraph.putEdge(1, 2);
+    tinyGraph.putEdge(2, 3);
+    tinyGraph.putEdge(2, 4);
+    tinyGraph.putEdge(3, 4);
+    tinyGraph.putEdge(3, 5);
   }
 
-  @Test
-  public void given_SourceIsZero_When_Count_Then_SixConnected() {
-    // Arrange
-    Graph<Integer> tinyGraph = createTinyGraph();
-    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, 0);
+  @ParameterizedTest(name = "{index} Source vertex: {0}, Connected vertex: {1}")
+  @CsvSource({"0, 6", "3, 6", "6 ,1"})
+  public void given_SourceVertex_When_Count_Then_HowManyConnected(int source, int expected) {
+    // Given
+    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, source);
 
-    // Act
+    // When
     int actual = dfs.count();
 
-    // Assert
-    int expected = 6;
+    // Then
     assertEquals(expected, actual);
   }
 
-  @Test
-  public void given_SourceIsSix_When_Count_Then_OneConnected() {
-    // Arrange
-    Graph<Integer> tinyGraph = createTinyGraph();
-    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, 6);
+  @ParameterizedTest(name = "{index} Given source vertex {0}, then is marked({1}) {2}?")
+  @CsvSource({"0, 4 ,true", "6, 4, false", "1, 1, true"})
+  public void given_SourceVertex_Then_IsTargetVertexMarked(
+      int source, int target, boolean expected) {
+    // Given
+    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, source);
 
-    // Act
-    int actual = dfs.count();
+    // When
+    boolean actual = dfs.marked(target);
 
-    // Assert
-    int expected = 1;
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  public void given_SourceIsZero_When_IsFourMarked_Then_True() {
-    // Arrange
-    Graph<Integer> tinyGraph = createTinyGraph();
-    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, 0);
-
-    // Act
-    boolean actual = dfs.marked(4);
-
-    // Assert
-    boolean expected = true;
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  public void given_SourceIsSix_When_IsFourMarked_Then_False() {
-    // Arrange
-    Graph<Integer> tinyGraph = createTinyGraph();
-    DepthFirstSearch<Integer> dfs = new DepthFirstSearch<Integer>(tinyGraph, 6);
-
-    // Act
-    boolean actual = dfs.marked(4);
-
-    // Assert
-    boolean expected = false;
+    // Then
     assertEquals(expected, actual);
   }
 }
